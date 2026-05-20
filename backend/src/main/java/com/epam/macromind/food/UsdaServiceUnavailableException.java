389e45231eff3cdbf4fe82757c6ff5406ad80811
Siarhei Liauko <siarhei_liauko@epam.com>
2026-05-20 16:13:34 +0200

    feat: add UsdaFoodClient

---
+++ backend/src/main/java/com/epam/macromind/food/UsdaServiceUnavailableException.java
@@ -0,0 +1,7 @@
+package com.epam.macromind.food;
+
+public class UsdaServiceUnavailableException extends RuntimeException {
+    public UsdaServiceUnavailableException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
